8864611dc315589ea99fb783b4ee3bc11462f9bd
mnklop <maxim210204@yandex.ru>
2020-10-01 23:33:55 +0300

    123

--- ft_additional_code.py
+++ ft_additional_code.py
@@ -5,6 +5,12 @@ import ft_reverse_code as rev
 
 def ft_additional_code(a):
     r = str(rev.ft_reverse_code(a))
-    n = int(r) % 10000000
+    n = r[1::]
     otv = r[0]
-    return otv + str(sss.ft_bin_num(tss.ft_rev_bin_num(n) + 1))
+    po = str(sss.ft_bin_num(tss.ft_rev_bin_num(int(n)) + 1))
+    if len(po) > 7:
+        po = "0000000"
+    if len(po) < 7:
+        while len(po) != 7:
+            po = "0" + po
+    return otv + po
--- ft_reverse_code.py
+++ ft_reverse_code.py
@@ -4,6 +4,8 @@ import ft_straight_code as pr
 def ft_reverse_code(a):
     p = str(pr.ft_straight_code(a))
     otv = p[0]
+    if a > 0:
+        return p
     for i in range(1, len(p)):
         if p[i] == "0":
             otv += "1"
